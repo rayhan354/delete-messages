6920df18adee894e5f3b18276a42e01852153537
rayhan354 <rayhan354@users.noreply.github.com>
2021-11-10 19:34:20 +0700

    Update log.py

--- lib/cogs/log.py
+++ lib/cogs/log.py
@@ -38,76 +38,8 @@ class Log(Cog):
 			self.bot.cogs_ready.ready_up("log")
 
 	@Cog.listener()
-	async def on_user_update(self, before, after):
-		if before.name != after.name:
-			embed = Embed(title="Username change",
-						  colour=after.colour,
-						  timestamp=datetime.utcnow())
-
-			fields = [("Before", before.name, False),
-					  ("After", after.name, False)]
-
-			for name, value, inline in fields:
-				embed.add_field(name=name, value=value, inline=inline)
-
-			await self.user_update.send(embed=embed)
-
-		if before.discriminator != after.discriminator:
-			embed = Embed(title="Discord tag change",
-						  description= f"{before.display_name}",
-						  colour=after.colour,
-						  timestamp=datetime.utcnow())
-
-			fields = [("Before", before.discriminator, False),
-					  ("After", after.discriminator, False)]
-
-			for name, value, inline in fields:
-				embed.add_field(name=name, value=value, inline=inline)
-
-			await self.user_update.send(embed=embed)
-
-		if before.avatar_url != after.avatar_url:
-			embed = Embed(title=f"Avatar change ( {before.display_name} )",
-						  description="New image is below, old is on right.",
-						  colour=self.avatar_update.guild.get_member(after.id).colour,
-						  timestamp=datetime.utcnow())
-
-			embed.set_thumbnail(url=before.avatar_url)
-			embed.set_image(url=after.avatar_url)
-
-			await self.avatar_update.send(embed=embed)
-
-	@Cog.listener()
-	async def on_member_update(self, before, after):
-		if before.display_name != after.display_name:
-			embed = Embed(title="Nickname change",
-						  colour=after.colour,
-						  timestamp=datetime.utcnow())
-
-			fields = [("Before", before.display_name, False),
-					  ("After", after.display_name, False)]
-
-			for name, value, inline in fields:
-				embed.add_field(name=name, value=value, inline=inline)
-
-			await self.user_update.send(embed=embed)
-
-		elif before.roles != after.roles:
-			embed = Embed(title="Role updates",
-						  description=f"{before.display_name}",
-						  colour=after.colour,
-						  timestamp=datetime.utcnow())
-
-			fields = [("Before", ", ".join([r.mention for r in before.roles]), False),
-					  ("After", ", ".join([r.mention for r in after.roles]), False)]
-
-			for name, value, inline in fields:
-				embed.add_field(name=name, value=value, inline=inline)
-
-			await self.role_update.send(embed=embed)
-
-	@Cog.listener()
 	async def on_message_edit(self, before, after):
+		await self.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="Genshin Impact SEA Tavern"))
 		if not after.author.bot:
 			if before.content != after.content:
 				embed = Embed(title="Message edit",
@@ -125,6 +57,7 @@ class Log(Cog):
 
 	@Cog.listener()
 	async def on_message_delete(self, message):
+		await self.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="Genshin Impact SEA Tavern"))
 		if not message.author.bot:
 			if not message.attachments:
 				embed = Embed(title="Message delete",
